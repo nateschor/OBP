04162c07d0982199342906ddb2d9d92aa71017c8
Nathan Schor <70981003+nateschor@users.noreply.github.com>
2023-02-03 14:21:22 -0500

    Save lags 1-5 at player-year level #3

--- code/01_prepare_lahman_data.R
+++ code/01_prepare_lahman_data.R
@@ -3,10 +3,7 @@ pacman::p_load(
   tidyverse,
   tidylog,
   Lahman,
-  lubridate,
   here,
-  slider,
-  ggridges,
   tsibble,
   tictoc
 )
@@ -17,11 +14,35 @@ path_master <- here("data/lahman/raw/master.csv")
 write_csv(Batting, path_batting)
 write_csv(Master, path_master)
 
+# https://www.smartfantasybaseball.com/tools/
+
 df_fg <- read_csv(here("data/fangraphs/raw/obp.csv")) %>% 
-  mutate(
-    Name = str_replace_all(Name, " ", "_") # better to fill empty space with "_" for merging
+  select(-birth_date) %>% 
+  rename(
+    "fgID" = playerid
   ) %>% 
   glimpse() 
+
+df_fg_OBP_longer <- df_fg %>% 
+  select(Name, fgID, starts_with("OBP")) %>% 
+  pivot_longer(., starts_with("OBP"), names_to = "yearID", values_to = "OBP", names_prefix = "OBP_") %>% 
+  mutate(
+    yearID = 2000 + as.integer(yearID)
+  ) %>% 
+  glimpse()
+
+df_fg_PA_longer <- df_fg %>% 
+  select(Name, fgID, starts_with("PA")) %>% 
+  pivot_longer(., starts_with("PA"), names_to = "yearID", values_to = "PA", names_prefix = "PA_") %>% 
+  mutate(
+    yearID = 2000 + as.integer(yearID)
+  ) %>% 
+  glimpse()
+
+df_fg_longer <- inner_join(df_fg_OBP_longer, df_fg_PA_longer, by = c("fgID", "yearID"))
+
+df_fg_longer %>% 
+  group_by(fgID, yearID) # confirm that dataset is now at the player-year level
 
 df_batting <- read_csv(path_batting) %>% 
   select(playerID, yearID, stint, G:GIDP) %>% 
@@ -31,7 +52,11 @@ df_batting %>%
   group_by(playerID, yearID, stint) # confirm that dataset is at the player-year-stint level 
 
 df_master <- read_csv(path_master) %>% 
-  select(playerID, nameFirst, nameLast, birthYear, birthMonth, birthDay) %>% 
+  select(playerID, bbrefID, nameFirst, nameLast) %>% 
+  glimpse()
+
+df_crosswalk <- read_csv(here("data/sfbb_player_id_crosswalk.csv")) %>% 
+  select(bbrefID = "BREFID", fgID = "IDFANGRAPHS") %>% 
   glimpse()
 
 df_batting_aggregated <- df_batting %>% 
@@ -40,10 +65,12 @@ df_batting_aggregated <- df_batting %>%
     across(G:GIDP, ~ sum(., na.rm = TRUE))
   ) %>% 
   ungroup() %>% 
-  left_join(df_master, by = "playerID") 
+  left_join(df_master, by = "playerID") %>% 
+  select(-playerID) %>% 
+  glimpse()
 
 df_batting_aggregated %>% 
-  group_by(playerID, yearID) # confirm that dataset is now at the player-year level
+  group_by(bbrefID, yearID) # confirm that dataset is now at the player-year level
 
 df_batting_stats <- df_batting_aggregated %>% 
   mutate(
@@ -51,69 +78,20 @@ df_batting_stats <- df_batting_aggregated %>%
     OBP = (H + BB + HBP) / (AB + BB + HBP + SF),
     AVG = H / AB,
     SLG = (X2B + (2 * X3B) + (3 * HR)) / AB + AVG,
-    birth_date = paste0(birthYear, birthMonth, birthDay) %>% ymd(),
-    Name = paste(nameFirst, nameLast, sep = "_")
   ) %>% 
   glimpse()
 
-df_plotting <- df_batting_stats %>% 
-  select(yearID, OBP, PA) %>% 
-  na.omit() %>% 
-  filter(between(OBP, .0001, .999)) %>% 
-  filter(between(yearID, 2000, 2020))
-    
-    
-ggplot(df_plotting) +
-  geom_density_ridges(aes(x = OBP/PA, y = factor(yearID)))
-
-df_fg %>% 
-  left_join(., df_batting_stats) %>% 
-  group_by()
-
-df_PA <- df_fg %>% 
-  select(Name:birth_date, starts_with("PA")) %>% 
-  pivot_longer(., starts_with("PA"), names_to = "Year", values_to = "PA", names_prefix = "PA_")
-
-df_OBP <- df_fg %>% 
-  select(Name:birth_date, starts_with("OBP")) %>% 
-  pivot_longer(., starts_with("OBP"), names_to = "Year", values_to = "OBP", names_prefix = "OBP_")
-
-df <- inner_join(df_OBP, df_PA) %>% 
-  filter(Year != 21) %>% 
-  mutate(
-    OBP_PA = OBP / PA
-  )
-
-ggplot(df, aes(x = OBP, y = Year)) +
-  geom_density_ridges()
-
-ggplot(df, aes(x = OBP_PA, y = Year)) +
-  geom_density_ridges() +
-  scale_x_continuous(limits = c(0, .005))
-
-
-temp_ <- df_batting_stats %>% 
-  arrange(playerID, yearID) 
-
-df_tsibble <- as_tsibble(temp_, key = playerID, index = yearID) %>% 
+df_tsibble <- as_tsibble(df_batting_stats, key = bbrefID, index = yearID) %>% 
   fill_gaps() %>% 
-  filter(yearID >= 1995) %>% 
-  arrange(playerID, yearID) %>% 
-  group_by(playerID)
-
-temp <- df_tsibble %>% 
-  
-  mutate(
-    lagged_HR = lag(HR)
-  )
+  arrange(bbrefID, yearID) %>% 
+  group_by(bbrefID)
 
 Grab_Lags <- function(tsibble_df, stat, num_lags) {
   
   tsibble_df %>% 
     transmute(
-      playerID, 
+      bbrefID, 
       yearID,
-      Name,
       "lagged_{stat}_{num_lags}":= lag(.data[[stat]], n = num_lags)
     ) 
   
@@ -131,7 +109,7 @@ df_combos <- expand_grid(
   number_of_lags = 1:5
 )
 
-names(df_tsibble)
+# 2 minutes
 
 tic()
 df_lags <- map2_dfc(df_combos$v_stats, df_combos$number_of_lags, function(x, y) {
@@ -142,12 +120,39 @@ toc()
 
 df_lags_cleaned <- df_lags %>% 
   as_tibble() %>% 
-  select(playerID = "playerID...1", yearID = "yearID...2", Name = "Name...3", contains("_"))
+  select(bbrefID = "bbrefID...1", yearID = "yearID...2", contains("_")) %>% 
+  glimpse()
 
 df_lags_cleaned %>% 
-  filter(Name == "Barry_Bonds") %>% 
-  select(yearID, OBP, contains("OBP"))
+  filter(bbrefID == "howarry01") %>% 
+  select(yearID, contains("OBP")) # sanity check
+
+df_lags_cleaned %>% 
+  group_by(bbrefID, yearID) # confirm at year-player level
+
+v_stats_to_rename <- names(df_batting_stats) == str_to_upper(names(df_batting_stats))
+
+df_batting_stats_renamed <- df_batting_stats %>% 
+  rename_with(., ~ paste0("cur_", .), c(-yearID, - bbrefID, -nameFirst, -nameLast)) # give all stats time horizon in name
+
+df_lahman_merged <- left_join(df_lags_cleaned, df_batting_stats_renamed, by = c("bbrefID", "yearID")) %>% 
+  glimpse()
+
+write_csv(df_lahman_merged, here("data/lahman/derived/df_batting_lag5.csv"))
 
 
-left_join(df_batting_stats, df_lags_cleaned, by = c("playerID", "yearID", "Name"))
+df_fg_longer <- df_fg %>% 
+  select(Name, birth_date, starts_with("OBP")) %>% 
+  pivot_longer(., starts_with("OBP"), names_to = "yearID", values_to = "OBP", names_prefix = "OBP_") %>% 
+  mutate(
+    yearID = 2000 + as.integer(yearID)
+  )
 
+left_join(df_all_lags, by = c("Name", "birth_date", "yearID"))
+
+v_21_players <- df_fg %>% 
+  pull("Name")
+
+df_all_lags %>% 
+  filter(Name %in% v_21_players) %>% 
+  distinct(playerID)
