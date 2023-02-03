ad3ba2b0c90696c79ac4b9d37ca224097e2debfd
Nathan Schor <70981003+nateschor@users.noreply.github.com>
2023-02-03 17:02:45 -0500

    Add ridge plots #3

--- code/02_plot_OBP.R
+++ code/02_plot_OBP.R
@@ -55,3 +55,45 @@ v_lags <- 1:5
 walk(v_lags, ~ Plot_Lags(df_lag_plot, ., save = TRUE))
 
 
+# Ridge Plots -------------------------------------------------------------
+
+median_2019 <- df_ridge_plot %>% 
+  filter(yearID == 2019) %>% 
+  pull(cur_OBP) %>% 
+  median(., na.rm = TRUE)
+
+Plot_Ridges <- function(start_year, save = FALSE) {
+  
+  df_ridge_plot <- df_lahman %>% 
+    filter(yearID %in% start_year:2020) %>% 
+    filter(between(cur_OBP, .001, .999)) %>% 
+    select(bbrefID, yearID, cur_OBP)
+  
+  p <- ggplot(df_ridge_plot, aes(x = cur_OBP, y = factor(yearID))) +
+    geom_density_ridges(fill = "#4477AA") +
+    scale_x_continuous(expand = expansion(0, 0)) +
+    scale_y_discrete(expand = expansion(0, 0)) +
+    labs(
+      x = "OBP",
+      y = "Season"
+    ) +
+    geom_vline(xintercept = median_2019, linetype = "dashed", color = "#CC6677", size = 1) +
+    theme_minimal()
+  
+  if(save == TRUE) {
+    
+    file_name <- paste0("ridge_", start_year, "_2020.png")
+    path_plot <- here("report/figures/", file_name)
+    
+    ggsave(plot = p, filename = path_plot)
+    
+  }
+  
+}
+
+v_ridge_start_years <- c(2000, 2013, 2016)
+
+walk(v_ridge_start_years, ~ Plot_Ridges(., save = TRUE))
+
+
+
