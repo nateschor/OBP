f6bb1f9dd4cb262d21530a5029f1a65c10d47b64
Nathan Schor <70981003+nateschor@users.noreply.github.com>
2023-02-04 09:21:17 -0500

    Issue3 perform eda (#5)
    * Save Batting and Master datasets #3
    
    * Calculate lags for all stats 1-5 years (code needs refactoring) #3
    
    * Save lags 1-5 at player-year level #3
    
    * Include 2021 data #3
    
    * Create 1-5 year lag plots #3
    
    * Add ridge plots #3
    
    * Add lag plots #3
    
    * Add here() call to loading data #3
    
    * Add OBP per PA ridge plot #3
    
    * Add code for correlation matrix and PCA #3
    
    * Save 2019 and 2020 OBP scatterplot #3
    
    * Include section headers and remove unused code #3
    
    * Add more comments to Lahman code #3 #5
    
    * Include 'OBP' in lag plot labels #3 #5

---
+++ code/01_prepare_lahman_data.R
@@ -0,0 +1,142 @@
+
+pacman::p_load(
+  tidyverse,
+  tidylog,
+  Lahman,
+  here,
+  tsibble,
+  tictoc
+)
+
+path_batting <- here("data/lahman/raw/batting.csv")
+path_people <- here("data/lahman/raw/people.csv")
+
+write_csv(Batting, path_batting)
+write_csv(People, path_people)
+
+# FanGraphs ---------------------------------------------------------------
+
+df_fg <- read_csv(here("data/fangraphs/raw/obp.csv")) %>% 
+  select(-birth_date) %>% 
+  rename(
+    "fgID" = playerid
+  ) %>% 
+  glimpse() 
+
+df_fg_OBP_longer <- df_fg %>% 
+  select(Name, fgID, starts_with("OBP")) %>% 
+  pivot_longer(., starts_with("OBP"), names_to = "yearID", values_to = "OBP", names_prefix = "OBP_") %>% 
+  mutate(
+    yearID = 2000 + as.integer(yearID)
+  ) %>% 
+  glimpse()
+
+df_fg_PA_longer <- df_fg %>% 
+  select(Name, fgID, starts_with("PA")) %>% 
+  pivot_longer(., starts_with("PA"), names_to = "yearID", values_to = "PA", names_prefix = "PA_") %>% 
+  mutate(
+    yearID = 2000 + as.integer(yearID)
+  ) %>% 
+  glimpse()
+
+df_fg_longer <- inner_join(df_fg_OBP_longer, df_fg_PA_longer, by = c("fgID", "yearID"))
+
+df_fg_longer %>% 
+  group_by(fgID, yearID) # confirm that dataset is now at the player-year level
+
+
+# Lahman ------------------------------------------------------------------
+
+df_batting <- read_csv(path_batting) %>% 
+  select(playerID, yearID, stint, G:GIDP) %>% 
+  glimpse()
+
+df_batting %>% 
+  group_by(playerID, yearID, stint) # confirm that dataset is at the player-year-stint level 
+
+df_people <- read_csv(path_people) %>% 
+  select(playerID, bbrefID, nameFirst, nameLast) %>% 
+  glimpse()
+
+df_crosswalk <- read_csv(here("data/sfbb_player_id_crosswalk.csv")) %>% # https://www.smartfantasybaseball.com/tools/
+  select(bbrefID = "BREFID", fgID = "IDFANGRAPHS") %>% 
+  glimpse()
+
+df_batting_aggregated <- df_batting %>% 
+  group_by(playerID, yearID) %>% 
+  summarize(
+    across(G:GIDP, ~ sum(., na.rm = TRUE))
+  ) %>% 
+  ungroup() %>% 
+  left_join(df_people, by = "playerID") %>% 
+  select(-playerID) %>% 
+  glimpse()
+
+df_batting_aggregated %>% 
+  group_by(bbrefID, yearID) # confirm that dataset is now at the player-year level
+
+df_batting_stats <- df_batting_aggregated %>% 
+  mutate(
+    PA = AB + BB + HBP + SF + SH,
+    OBP = (H + BB + HBP) / (AB + BB + HBP + SF),
+    AVG = H / AB,
+    SLG = (X2B + (2 * X3B) + (3 * HR)) / AB + AVG,
+  ) %>% 
+  glimpse()
+
+df_tsibble <- as_tsibble(df_batting_stats, key = bbrefID, index = yearID) %>% 
+  fill_gaps() %>% # make missing seasons explicit instead of implicit so that previous row = last played season
+  arrange(bbrefID, yearID) %>% 
+  group_by(bbrefID)
+
+Grab_Lags <- function(tsibble_df, stat, num_lags) {
+  
+  tsibble_df %>% 
+    transmute(
+      bbrefID, 
+      yearID,
+      "lagged_{stat}_{num_lags}":= lag(.data[[stat]], n = num_lags)
+    ) 
+}
+
+v_stats <- df_tsibble %>% 
+  ungroup() %>%
+  as_tibble() %>% 
+  select(G:GIDP, PA, OBP, AVG, SLG) %>%
+  names()
+
+df_combos <- expand_grid(
+  v_stats,
+  number_of_lags = 1:5
+)
+
+# 2.5 minutes
+
+tic()
+df_lags <- map2_dfc(df_combos$v_stats, df_combos$number_of_lags, function(x, y) {
+  print(x) # Make sure code is running
+  Grab_Lags(df_tsibble, x, y)
+}) 
+toc()
+
+df_lags_cleaned <- df_lags %>% 
+  as_tibble() %>% 
+  select(bbrefID = "bbrefID...1", yearID = "yearID...2", contains("_")) %>% 
+  glimpse()
+
+df_lags_cleaned %>% 
+  filter(bbrefID == "howarry01") %>% 
+  select(yearID, contains("OBP")) # sanity check
+
+df_lags_cleaned %>% 
+  group_by(bbrefID, yearID) # confirm at year-player level
+
+v_stats_to_rename <- names(df_batting_stats) == str_to_upper(names(df_batting_stats)) # stats are in all caps, other variables are not all caps
+
+df_batting_stats_renamed <- df_batting_stats %>% 
+  rename_with(., ~ paste0("cur_", .), c(-yearID, - bbrefID, -nameFirst, -nameLast)) # give all stats time horizon in name, easier to remove for testing
+
+df_lahman_merged <- left_join(df_lags_cleaned, df_batting_stats_renamed, by = c("bbrefID", "yearID")) %>% 
+  glimpse()
+
+write_csv(df_lahman_merged, here("data/lahman/derived/df_batting_lag5.csv"))
---
+++ code/02_plot_OBP.R
@@ -0,0 +1,145 @@
+
+pacman::p_load(
+  tidyverse,
+  tidylog,
+  here,
+  ggridges
+)
+
+df_lahman <- read_csv(here("data/lahman/derived/df_batting_lag5.csv")) %>% 
+  glimpse()
+
+# Lag Plots ---------------------------------------------------------------
+
+df_lag_plot <- df_lahman %>% 
+  filter(yearID %in% 2015:2020) %>% 
+  filter(
+    if_all(contains("OBP"), ~ between(., .001, .999))
+  ) %>% 
+  select(bbrefID, yearID, contains("OBP"))
+
+Plot_Lags <- function(plotting_data, num_lag, save = FALSE) {
+  
+  x_axis <- paste0("lagged_OBP_", num_lag)
+  x_axis_title <- paste0(num_lag, "-year Lag of OBP")
+  
+  p <- ggplot(data = df_lag_plot, aes_string(x = x_axis, y = "cur_OBP")) +
+    geom_point(alpha = .3, size = 1.5) +
+    geom_smooth(se = FALSE, color = "#4477AA", size = 1) +
+    geom_abline(slope = 1, linetype = "dashed", color = "#CC6677", size = 1) +
+    scale_x_continuous(limits = c(0, .7), expand = expansion(0, 0)) +
+    scale_y_continuous(limits = c(0, .7), expand = expansion(0, 0)) +
+    labs(
+      x = x_axis_title,
+      y = "Current Season OBP"
+    ) +
+    theme_minimal() +
+    theme(
+      panel.grid.minor = element_blank()
+    )
+  
+  if (save) {
+    
+    file_name <- paste0("lag_", num_lag, ".png")
+    path_plot <- here("report/figures/", file_name)
+    
+    ggsave(plot = p, filename = path_plot)
+    
+  }
+  
+}
+
+v_lags <- 1:5
+
+walk(v_lags, ~ Plot_Lags(df_lag_plot, ., save = TRUE))
+
+
+# 2019 vs. 2020 to see if relationship persists ---------------------------
+
+df_2020 <- df_lahman %>% 
+  filter(yearID == 2020) %>% 
+  select(bbrefID, yearID, cur_OBP, lagged_OBP_1) %>% 
+  filter(
+    if_all(contains("OBP"), ~ between(., .001, .999))
+  )
+
+p <- ggplot(data = df_2020, aes(x = lagged_OBP_1, y = cur_OBP)) +
+  geom_point(alpha = .3, size = 1.5) +
+  geom_smooth(se = FALSE, color = "#4477AA", size = 1) +
+  geom_abline(slope = 1, linetype = "dashed", color = "#CC6677", size = 1) +
+  scale_x_continuous(limits = c(0, .7), expand = expansion(0, 0)) +
+  scale_y_continuous(limits = c(0, .7), expand = expansion(0, 0)) +
+  labs(
+    x = "2019 OBP",
+    y = "2020 OBP"
+  ) +
+  theme_minimal() +
+  theme(
+    panel.grid.minor = element_blank()
+  )
+
+ggsave(plot = p, filename = here("report/figures/2019_2020_lag.png"))
+
+# Ridge Plots -------------------------------------------------------------
+
+median_2019 <- df_ridge_plot %>% 
+  filter(yearID == 2019) %>% 
+  pull(cur_OBP) %>% 
+  median(., na.rm = TRUE)
+
+Plot_Ridges <- function(start_year, save = FALSE) {
+  
+  df_ridge_plot <- df_lahman %>% 
+    filter(yearID %in% start_year:2020) %>% 
+    filter(between(cur_OBP, .001, .999)) %>% 
+    select(bbrefID, yearID, cur_OBP)
+  
+  p <- ggplot(df_ridge_plot, aes(x = cur_OBP, y = factor(yearID))) +
+    geom_density_ridges(fill = "#4477AA") +
+    scale_x_continuous(expand = expansion(0, 0)) +
+    scale_y_discrete(expand = expansion(0, 0)) +
+    labs(
+      x = "OBP",
+      y = "Season"
+    ) +
+    geom_vline(xintercept = median_2019, linetype = "dashed", color = "#CC6677", size = 1) +
+    theme_minimal()
+  
+  if(save == TRUE) {
+    
+    file_name <- paste0("ridge_", start_year, "_2020.png")
+    path_plot <- here("report/figures/", file_name)
+    
+    ggsave(plot = p, filename = path_plot)
+    
+  }
+  
+}
+
+v_ridge_start_years <- c(2000, 2013, 2016)
+
+walk(v_ridge_start_years, ~ Plot_Ridges(., save = TRUE))
+
+
+df_ridge_plot <- df_lahman %>% 
+  filter(yearID %in% 2016:2020) %>% 
+  filter(between(cur_OBP, .001, .999)) %>% 
+  transmute(
+    bbrefID, 
+    yearID, 
+    OBP_per_PA = cur_OBP / cur_PA
+)
+
+p <- ggplot(df_ridge_plot, aes(x = OBP_per_PA, y = factor(yearID))) +
+  geom_density_ridges(fill = "#4477AA") +
+  scale_x_continuous(limits = c(0, .01), expand = expansion(0, .001)) +
+  scale_y_discrete(expand = expansion(0, 0)) +
+  labs(
+    x = "OBP per PA",
+    y = "Season"
+  ) +
+  theme_minimal()
+
+ggsave(plot = p, filename = here("report/figures/ridge_2016_2020_OBP_per_PA.png"))
+
+
