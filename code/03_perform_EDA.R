560f2dca441c31d55048ed551b729f7aa9eb6ae7
Nathan Schor <70981003+nateschor@users.noreply.github.com>
2023-02-05 18:46:10 -0500

    Calculate NA statistics for provided dataset #4

--- code/03_perform_EDA.R
+++ code/03_perform_EDA.R
@@ -27,3 +27,34 @@ df_lahman %>%
   select(contains("_")) %>% 
   na.omit() %>% 
   DataExplorer::plot_prcomp(., variance_cap = .60)
+
+df_fg <- read_csv("data/fangraphs/raw/obp.csv") %>% 
+  mutate(
+    playerid = as.character(playerid)
+  ) %>% 
+  select(-OBP_21, -PA_21)
+
+
+df_fg %>% 
+  map_dbl(~ sum(is.na(.)))
+
+skimr::skim(df)
+
+df_fg %>% 
+  select(playerid, contains("OBP")) %>% 
+  pivot_longer(-playerid) %>%
+  group_by(playerid) %>% 
+  summarize(
+    num_missing = sum(is.na(value))
+  ) %>% 
+  filter(num_missing >1)
+
+df_fg %>% 
+  select(playerid, num_range("PA_", 16:20)) %>% 
+  filter(
+    PA_16 >= 100,
+    PA_17 >= 100,
+    PA_18 >= 100,
+    PA_19 >= 100,
+    PA_20 >= 30,
+  )
